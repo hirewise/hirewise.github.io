b19410c8c5fd4c2e6decc99fccd3ed35f6397153
vishnu667 <vishnu667@gmail.com>
2020-05-19 06:10:45 +0530

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,99 @@
+module.exports = function (s) {
+   s.initConfig({
+      pkg: s.file.readJSON("package.json"),
+      uglify: {
+         options: {
+            banner: ""
+         },
+         build: {
+            src: "src/**/*.js",
+            dest: "build/js/<%= pkg.name %>.min.js"
+         }
+      },
+      copy: {
+         main: {
+            files: [{
+               src: "src/index.html",
+               dest: "build/index.html",
+               clone: "tmp"
+            }, {
+               cwd: "src/images/",
+               src: ["*"],
+               dest: "build/images/",
+               expand: true
+            }]
+         }
+      },
+      watch: {
+         options: {
+            livereload: true
+         },
+         scripts: {
+            files: ["src/**/*", "src/*"],
+            tasks: ["build"]
+         }
+      },
+      clean: ["build"],
+      connect: {
+         server: {
+            options: {
+               livereload: true,
+               port: 9e3,
+               base: "build"
+            }
+         }
+      },
+      cssmin: {
+         options: {
+            report: "gzip",
+            root: "build/css/"
+         },
+         target: {
+            src: ["build/css/style.css"],
+            dest: "build/css/style.css"
+         }
+      },
+      uncss: {
+         options: {
+            htmlroot: "src"
+         },
+         dist: {
+            files: {
+               "build/css/style.css": ["src/index.html"]
+            }
+         }
+      },
+      processhtml: {
+         options: {
+            strip: true
+         },
+         dist: {
+            files: {
+               "build/index.html": ["src/index.html"]
+            }
+         }
+      },
+      htmlmin: {
+         dist: {
+            options: {
+               removeComments: true,
+               collapseWhitespace: true
+            },
+            files: {
+               "build/index.html": "build/index.html"
+            }
+         }
+      },
+      "gh-pages": {
+         options: {
+            base: "build",
+
+            user: {
+               name: "vishnu667",
+               email: "vishnu667@gmail.com"
+            }
+         },
+         src: ["**"]
+      }
+   }), s.loadNpmTasks("grunt-contrib-uglify"), s.loadNpmTasks("grunt-contrib-copy"), s.loadNpmTasks("grunt-contrib-connect"), s.loadNpmTasks("grunt-contrib-watch"), s.loadNpmTasks("grunt-contrib-clean"), s.loadNpmTasks("grunt-contrib-cssmin"), s.loadNpmTasks("grunt-uncss"), s.loadNpmTasks("grunt-processhtml"), s.loadNpmTasks("grunt-contrib-htmlmin"), s.loadNpmTasks("grunt-gh-pages"), s.registerTask("build", ["clean", "uncss", "uglify", "copy", "processhtml"]), s.registerTask("dist", ["clean", "uncss", "uglify", "cssmin", "copy", "processhtml", "htmlmin"]), s.registerTask("serve", ["build", "connect:server", "watch"]), s.registerTask("default", ["serve"]), s.registerTask("publish", ["dist", "gh-pages"])
+};
---
+++ src/js/landingpage.js
@@ -0,0 +1,10 @@
+AOS.init({
+    offset: 120, 
+    delay: 0,
+    duration: 1200, 
+    easing: 'ease', 
+    once: true, 
+    mirror: false, 
+    anchorPlacement: 'top-bottom', 
+    disable: "mobile"
+  });
